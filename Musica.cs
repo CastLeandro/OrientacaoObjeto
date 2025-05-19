2598138faef1b57bcd965517ee72149c6bbfd761
CastLeandro <leandro.castilione@cotefacil.com>
2025-05-19 17:43:23 -0300

    Finalizando treino de orientação a objeto

--- Musica.cs
+++ Musica.cs
@@ -1,8 +1,35 @@
 ﻿class Musica
 {
-    string nome;
-    string artista;
-    int duracao;
-    bool disponivel;
+    public Musica(Banda artista, string nome)
+    {
+        Artista = artista;
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+    public Banda Artista { get; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => $"A música {Nome} pertence a banda {Artista.Nome}";
+
+    public void ExibirFicha()
+    {
+        Console.WriteLine($"Descrição: {DescricaoResumida}");
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duracao: {Duracao}");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponivel no plano");
+        }
+        else
+        {
+            Console.WriteLine("Adqura o plano");
+        }
+    }
+    public void exibirArtistaNome()
+    {
+        Console.WriteLine($"O nome do artista é {Artista}, e o nome da música: {Nome}");
+    }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿Album albumDoQueen = new Album("A night at the opera");
+Banda queen = new Banda("Queen");
+
+Musica musica1 = new Musica(queen, "Love of my life")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(queen, "Bohemian Rhpsody")
+{
+    Duracao = 354,
+    Disponivel = true,
+};
+
+
+albumDoQueen.AdicionarMusica(musica1);
+albumDoQueen.AdicionarMusica(musica2);
+
+albumDoQueen.ExibirMusicasDoAlbum();
+queen.AdicionarAlbum(albumDoQueen);
+queen.ExibirDiscografia();
